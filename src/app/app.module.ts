9caa0c84c9c6a84ab2b3850ab4abcf5ba0262a59
weslleyg <wesley.dipaulatorres@hotmail.com>
2021-07-30 12:07:01 -0300

    Developing nav bar and adding routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,30 @@ import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list/course-list.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 import { StarComponent } from './star/star.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
     StarComponent,
-    ReplacePipe
+    ReplacePipe,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: "",
+        redirectTo: "courses",
+        pathMatch: "full"
+      },
+      {
+        path: "courses",
+        component: CourseListComponent
+      }
+    ]),
     FormsModule,
   ],
   providers: [],
