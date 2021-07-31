3775537c86ea5d4e5e4099f3793a19e8da120ba0
weslleyg <wesley.dipaulatorres@hotmail.com>
2021-07-31 18:42:33 -0300

    Update and save course with http client module

--- src/app/courses/course-info/course-info.component.ts
+++ src/app/courses/course-info/course-info.component.ts
@@ -25,7 +25,10 @@ export class CourseInfoComponent implements OnInit {
   }
 
   save(): void {
-    this.courseService.save(this.course);
+    this.courseService.save(this.course).subscribe({
+      next: courseResponse => console.log("Saved with success", courseResponse),
+      error: err => console.log("Error", err)
+    });
   }
 
 }
